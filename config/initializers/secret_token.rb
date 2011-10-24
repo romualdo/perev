e3a53c9f041e484d018edb54babf934042f5b369
Romualdo <bondarchuk.iym@gmail.com>
2011-10-24 14:13:34 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perev::Application.config.secret_token = '682de2253311fbb106148a57c99982399751e223686e1cfac1287b422d082ccfd61be8c4c036015758e4f896767b1afdb60fd2453590026b0a8aac9b214e64bf'
